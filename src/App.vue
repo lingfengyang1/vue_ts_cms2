2bff2b1f767c388691a6c731744d37de18dcaf1d
axihh <axihh134@gmail.com>
2025-09-15 10:29:17 +0800

    第五次提交

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import zhCn from "element-plus/es/locale/lang/zh-cn"
 
 <template>
   <!-- 让默认的英语修改成中文界面(比如我们的日期选项卡以及我们的底部分页器) 嗨嗨嗨 你他妈 我他妈 -->
+  <!-- 嘿嘿嘿 -->
   <el-config-provider :locale="zhCn">
     <router-view />
   </el-config-provider>
